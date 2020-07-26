7ca4265e9ad3a6bc535fdb20412dc2f8f7c7f45b
Vilson Borges <vilsonborges98@gmail.com>
2020-07-26 19:32:37 -0300

    styling listing

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -4,7 +4,20 @@ import { Keyboard } from 'react-native';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container, Form, Input, SubmitButton } from './styles';
+import {
+  Container,
+  Form,
+  Input,
+  SubmitButton,
+  List,
+  User,
+  Avatar,
+  Name,
+  Bio,
+  ProfileButton,
+  ProfileButtonText,
+}
+from './styles';
 
 
 export default class Main extends Component {
@@ -52,6 +65,20 @@ export default class Main extends Component {
             <Icon name="add" size={25} color="#FFF"/>
           </SubmitButton>
         </Form>
+        <List
+          data={users}
+          keyExtractor={user => user.login}
+          renderItem={({item}) => (
+            <User>
+              <Avatar source={{ uri: item.avatar}}/>
+              <Name>{item.name}</Name>
+              <Bio>{item.bio}</Bio>
+              <ProfileButton onPress={() => {}}>
+                <ProfileButtonText>Ver Perfil</ProfileButtonText>
+              </ProfileButton>
+            </User>
+          )}
+        />
       </Container>
     );
   }
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components/native';
 
-import { RectButton } from 'react-native-gesture-handler';
+import { RectButton, View, Text, Image } from 'react-native-gesture-handler';
 
 export const Container = styled.View`
   flex: 1;
@@ -34,4 +34,59 @@ export const SubmitButton = styled(RectButton)`
   padding: 0 12px;
 `;
 
+export const List = styled.FlatList.attrs({
+  showsVerticalScrollIndicator: false,
+})`
+  margin-top: 20px;
+`;
+
+  export const User = styled.View`
+    align-items: center;
+    margin: 0 20px 30px;
+    border-radius: 12px;
+    background: #DDD;
+  `;
+
+  export const Avatar = styled.Image`
+    margin-top: 4px;
+    width: 64px;
+    height: 64px;
+    border-radius: 32px;
+    background: #eee;
+  `;
+
+  export const Name = styled.Text`
+    font-size: 14px;
+    color: #333;
+    font-weight: bold;
+    margin-top: 4px;
+    text-align: center;
+  `;
+
+  export const Bio = styled.Text.attrs({
+    numberOfLines: 2
+  })`
+    font-size: 13px;
+    line-height: 18px;
+    color: #999;
+    margin-top: 5px;
+    text-align: center;
+  `;
+
+  export const ProfileButton = styled(RectButton)`
+    background: #7159c1;
+    margin-top: 10px;
+    align-self: stretch;
+    border-radius: 4px;
+    justify-content: center;
+    align-items: center;
+    height: 36px;
+  `;
+
+  export const ProfileButtonText = styled.Text`
+    font-size: 18px;
+    font-weight: bold;
+    color: #FFF;
+    text-transform: uppercase;
+  `;
 
