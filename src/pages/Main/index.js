c211d5d3948f1c27d72262372337f2db31c88f93
Vilson Borges <vilsonborges98@gmail.com>
2020-07-26 15:26:53 -0300

    setting stuled-components for react native

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Main(){
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <Container >
       <Text>Main Screen</Text>
-    </View>
+    </Container>
   );
 }
 
